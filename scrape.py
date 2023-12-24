bbbbd69654a64fe24583d2c7b3726384e2ad15fc
TerminalC3 <margakumars10@gmail.com>
2023-12-24 16:47:50 +0530

    Updated to extract product name, price and other information

--- scrape.py
+++ scrape.py
@@ -7,22 +7,53 @@ def extract_information(url):
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
         
+        # Extract product name
+        name = soup.find(class_='B_NuCI').text
+
+        # Extract product price
+        current_price = soup.find(class_='_30jeq3 _16Jk6d').text
+
+        # Extract product original price
+        original_price = soup.find(class_='_3I9_wc _2p6lqe').text
+
+        # Extract product discount
+        discount = soup.find(class_='_3Ay6Sb _31Dcoz').text
+
+        # Extract product rating
+        rating = soup.find(class_='_3LWZlK').text
+
+        # Extract product rating count
+        rating_count = soup.find(class_='row _2afbiS').text[:-1]
+
+        # Extract product review count
+        review_count = soup.find_all(class_='row _2afbiS')[1].text
+
+
         # Extract specification type
         specification_name = soup.find_all(class_='_1hKmbr')
 
         # Extract specificaion detail
         specification_detail = soup.find_all(class_='_21lJbe')
         
-        i=0
-        
-        # Add each specification to a dictionary
-        specification_dict = {}
+        # Store the extracted information in a dictionary
+        detail_dict = {}
+
+        detail_dict['Name'] = name
+        detail_dict['Current_price'] = current_price
+        detail_dict['Original_price'] = original_price
+        detail_dict['Discount'] = discount
+        detail_dict['Rating'] = rating
+        detail_dict['Rating_count'] = rating_count
+        detail_dict['Review_count'] = review_count
+
+                
+        # Add each specification to the dictionary        
         for i in range(len(specification_name)):
             key = specification_name[i].text
             value = specification_detail[i].text
-            specification_dict[key] = value
+            detail_dict[key] = value
         
-        return specification_dict
+        return detail_dict
         
 
     else:
