09d211d37e40a672045dbeb9d8f2bdb2a7136820
TerminalC3 <margakumars10@gmail.com>
2023-12-25 21:26:43 +0530

    Added text to speech

--- speak.py
+++ speak.py
@@ -9,10 +9,12 @@ def speaker(specificaion):
     
     if (specification_name == '0'):
         text = "Sorry, I could not find the specification you were looking for."
-    
+        # Display in UI
+
     else:
         text = f"The {specification_name} is {specification_detail}"
-
+        # Display in UI
+        
     engine.say(text)
 
     engine.runAndWait()
