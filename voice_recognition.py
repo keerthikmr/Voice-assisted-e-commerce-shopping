c982661bd6607f79d72c67062a26f425028e730c
TerminalC3 <margakumars10@gmail.com>
2023-12-25 21:27:36 +0530

    Modified to give text output to app

--- voice_recognition.py
+++ voice_recognition.py
@@ -3,8 +3,9 @@ import speech_recognition as sr
 def recognize_speech():
 
     recognizer = sr.Recognizer()
+    mic = sr.Microphone()
 
-    with sr.Microphone() as source:
+    with mic as source:
         print("Say something...")
         recognizer.adjust_for_ambient_noise(source)
         audio_data = recognizer.listen(source, timeout=5)
@@ -13,9 +14,7 @@ def recognize_speech():
         print("Recognizing...")
         text = recognizer.recognize_google(audio_data)
         print(text)
-
+        return text
+    
     except sr.UnknownValueError:
-        print("Could not recognize what you said. Please try again.")
-
-if __name__ == "__main__":
-    recognize_speech()
+        return None
