97e9fc81501f8fa9d246e37eb12721add54ca8a8
TerminalC3 <margakumars10@gmail.com>
2023-12-26 22:09:56 +0530

    Extracted text from app and simplified speech output

--- speak.py
+++ speak.py
@@ -1,19 +1,8 @@
 import pyttsx3
 import similarity_matching as sm
 
-def speaker(specificaion):
+def speaker(text):
     engine = pyttsx3.init()
-    
-    specification_name = specificaion[0]
-    specification_detail = specificaion[1]
-    
-    if (specification_name == '0'):
-        text = "Sorry, I could not find the specification you were looking for."
-        # Display in UI
-
-    else:
-        text = f"The {specification_name} is {specification_detail}"
-        # Display in UI
         
     engine.say(text)
 
